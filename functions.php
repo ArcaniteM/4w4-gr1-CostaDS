c6ac65f6681024bb52408c55d72b7272c359b326
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-01-26 11:53:47 -0500

    commit3

--- functions.php
+++ functions.php
@@ -5,7 +5,7 @@
 function enfiler_css(){
 
 
-wp_enqueue_style('4w4-gr2-principal', // identificateur
+wp_enqueue_style('4w4-gr1-principal', // identificateur
         get_template_directory_uri() . '/style.css', // adresse url de style.css
         array(), // définir les dépendances
         filemtime(get_template_directory() . '/style.css'), // le calcul de la version du fichier css
--- header.php
+++ header.php
@@ -4,6 +4,6 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>mon 4W4</title>
+    <title>Mon Projet 4W4</title>
     <?php wp_head(); ?>
 </head>
