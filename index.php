8509717a18829662ff81be040ed8c97e063e6a5a
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-02-02 09:53:41 -0500

    have-posts

--- index.php
+++ index.php
@@ -3,6 +3,13 @@
 <body>
     <main>
         <h1>Bienvenue sur 4W4</h1>
+        <?php if(have_posts()):
+                    while(have_posts()):
+                        the_post(); //extraire un objet post 
+                        the_title(); //affiche le titre du post
+                        
+                    endwhile;
+                endif; ?>
     </main>
     <?php get_footer(); ?>
 </body>
