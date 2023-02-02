6c72b047972ce1efbe333ece2a6bbe65d45994ab
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-02-02 11:57:20 -0500

    entete scss

--- single.php
+++ single.php
@@ -1,24 +1,24 @@
 <?php 
 /**
- * 
- * 
- * 
+ * single.php est le modèle par défaut qui affiche un article
  */
 ?>
 
-<main>
-        <pres>front-page.php</pres>
+<?php get_header(); ?>
+    <main>
+        <pre>single.php</pre>
         <h1>Bienvenue sur 4W4</h1>
-        <?php if(have_posts()):
-                while(have_posts()):the_post();   ?> 
-                <article>
-                    <h3><?php the_title(); ?></h3>
-                    <?php the_excerpt(); ?>
-                    <?php the_content(); ?>
-                    
-                </article>
-                <hr>
-        <?php endwhile; ?>
-        <?php endif; ?>
-         
-</main>
+    <?php if(have_posts()):
+            while (have_posts()): the_post(); ?>
+            <article>
+                <h3><?php the_title(); ?></h3>
+                <?php the_content(); ?>
+            </article>
+            <hr>
+            <?php endwhile; ?>
+           <?php  endif; ?>
+    </main>
+    
+    <?php get_footer(); ?>
+</body>
+</html>
