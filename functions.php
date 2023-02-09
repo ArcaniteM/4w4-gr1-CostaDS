974894fcee132eb3ad69843e1c308c0430d770ab
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-02-09 11:45:00 -0500

    beautify scss

--- functions.php
+++ functions.php
@@ -1,17 +1,15 @@
 <?php
 /**
 * L'ensemble des fonctions du thème
-* comment php
 */
-function enfiler_css(){
-
-
-wp_enqueue_style('4w4-gr1-principal', // identificateur
+function enfiler_css() {
+wp_enqueue_style('4w4-gr1-principal', // id
         get_template_directory_uri() . '/style.css', // adresse url de style.css
         array(), // définir les dépendances
         filemtime(get_template_directory() . '/style.css'), // le calcul de la version du fichier css
         'all'); // media
-}
+}        
+
 add_action( 'wp_enqueue_scripts', 'enfiler_css' );  
 
 /* -------------------------------------- Enregitrement des menus */
@@ -23,9 +21,10 @@ function enregistre_menus(){
 }
 add_action( 'after_setup_theme', 'enregistre_menus', 0 );
 
-//add theme support
+/* -------------------------------------- add_theme_suport */
+
 add_theme_support( 'title-tag' );
 add_theme_support( 'custom-logo', array(
     'height' => 150,
-    'width'  => 150,
+    'width'  => 150
 ) );
