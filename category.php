b56c08007f043675e1d82ab8ca90a513b72ff7de
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-02-23 10:55:23 -0500

    categorie update

--- category.php
+++ category.php
@@ -1,23 +1,30 @@
 <?php /** category par defaut du theme */ ?>
 <?php get_header(); ?>
 
-<main>
-        <pre>front-page.php</pre>
-        <h1>Bienvenue sur 4W4</h1>
-        <section class="blocflex">
-            <?php if(have_posts()):
-                while (have_posts()): the_post(); ?>
-                <article>
-                    <h3><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h3>
-                    <?php // get_the_excerpt() ?>
-                    <hr>
-                    <?= wp_trim_words(get_the_excerpt(), 10, " ... "); ?>
-                </article>
-                <?php endwhile; ?>
-            <?php  endif; ?>
-        </section>
-    </main>
-
+<main class="site__main">
+   <section class="blocflex">
+      <?php
+      $category = get_queried_object();
+      // permet d utiliser la nouevlle requette
+      $args = array(
+         'category_name' => $category->slug,
+         'orderby' => 'title',
+         'order' => 'ASC'
+      );
+      //creation d une nouvelle requette
+      $query = new WP_Query( $args );
+      // tout le reste de l extraction est base sur wp_query 
+      if ( $query->have_posts() ) :
+         while ( $query->have_posts() ) : $query->the_post(); ?>
+            <article>
+               <h2><a href="<?php the_permalink(); ?>"> <?= get_the_title(); ?></a></h2>
+               <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+            </article>
+         <?php endwhile; ?>
+      <?php endif;
+      wp_reset_postdata();?>
+   </section>
+</main>           
     <?php get_footer(); ?>
 	
 </body>
