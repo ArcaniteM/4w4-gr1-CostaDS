400f59af9315373a525a471ea58055b18ef2d97e
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-03-14 11:50:58 -0400

    menu burger 14 mars

--- header.php
+++ header.php
@@ -4,32 +4,44 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Mon Projet 4W4</title>
-    
+    <title>mon 4W4</title>
     
     <?php wp_head(); ?>
-    
-    <body class="site <?php echo(is_front_page()?"no-aside":"") ?>">
-<body>
+</head>
+<?php
+$nouvel_class="";
+ if (is_front_page()) {
+    $nouvel_class = "no-aside";
+ }?> 
+
+<body class="site <?= $nouvel_class ?>  ">
     <header class="site__entete">
         <section class="entete__nav">
             <?php the_custom_logo(); ?> 
-            <?php  wp_nav_menu(array( 
-                            "menu" => "entete",
-                            "container" => "nav",
-                            "container_class" => "menu__entete"
+            <div class="menu__recherche">
+                <input type="checkbox"  id="chkBurger"> 
+                <?php  wp_nav_menu(array( 
+                                "menu" => "entete",
+                                "container" => "nav",
+                                "container_class" => "menu__entete"
 
-                        )); ?>
-            <?php get_search_form(); ?>
+                            )); ?>
+                <?= get_search_form(); ?>  
+                
+                <label class="burger" for="chkBurger">
+                    <img src="https://s2.svgbox.net/hero-outline.svg?ic=menu&color=000" width="32" height="32">
+                </label> 
+            </div>         
         </section>
         <h1 class="site__titre"><a  href="<?= bloginfo('url'); ?>"><?= bloginfo('name'); ?></a></h1>
         <h2 class="site__description"><?= bloginfo('description'); ?></h2>
 </header>
 
 <?php 
-if(! is_front_page()){
-    get_template_part("template-parts/aside");
+if (! is_front_page()){
+    get_template_part("template-parts/aside"); 
 }
+
 ?>
 
     
