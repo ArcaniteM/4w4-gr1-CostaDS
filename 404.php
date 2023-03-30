2b75cb1ea56643356e1c87f2517b8dcdec0c0076
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-03-30 09:24:06 -0400

    read me start et titre et message sur la page 404

--- 404.php
+++ 404.php
@@ -5,7 +5,7 @@
     <pres>404.php</pres>
         <h1>Bienvenue sur 4W4</h1>
         <h1>Erreur 404</h1>
-        
+        <p>Page introuvable, veuillez tenter une recherche</p>
     </main>
     <?php get_footer(); ?>
 </body>
