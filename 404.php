ffec75fb5ae5b3a42711d627fd1b1bf541cc6b21
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-03-30 11:13:04 -0400

    stylizé les choix de cours et les notes de cours

--- 404.php
+++ 404.php
@@ -2,41 +2,47 @@
 <?php get_header(); ?>
 
     <main class="site__main page404">
-
-    <div class="content404" id="contentId404">
-        <pres>404.php</pres>
-        <h1>Bienvenue sur 4W4</h1>
-        <h1>Erreur 404</h1>
-        <p class="msg404">Page introuvable, veuillez tenter une recherche</p>
-            <div class="menu__recherche recherche404"> 
-                <?= get_search_form(); ?>        
-                </label> 
-            </div> 
         
-        <div class="menu__array">
-            <?php 
-                // $menu peut prendre les valeurs : "note-4w4" ou "cours"
-                $menu1 = "note-4w4";
-                $menu2 = "cours";
+        <div class="content404">
+            <pres>404.php</pres>
+            <h1>Bienvenue sur 4W4</h1>
+            <h1>Erreur 404</h1>
+            <p class="msg404">Page introuvable, veuillez tenter une recherche</p>
+                <div class="menu__recherche recherche404"> 
+                    <?= get_search_form(); ?>        
+                    </label> 
+                </div> 
+            
+            <div class="menu__array">
+                <?php 
+                    // $menu peut prendre les valeurs : "note-4w4" ou "cours"
+                    $menu2 = "note-4w4";
+                    $menu1 = "cours";?>
+                    <div class="titleNav_404">
+                        <?php echo "Nos choix de cours"; ?>
+                    </div>
+                    
+                <?php 
+                        wp_nav_menu(array(
+                        "menu"=>$menu1,
+                        "container"=>"nav"
+                    ))
+                ?>
+                    
                 
-                echo $menu1;
-                wp_nav_menu(array(
-                    "menu"=>$menu1,
-                    "container"=>"nav"
-                ))
-            ?>
-                
-            <?php
-                echo $menu2;
-                wp_nav_menu(array(
-                    "menu"=>$menu2,
-                    "container"=>"nav"
-                ))
-                
-            ?>
+                    <div class="titleNav_404">
+                        <?php echo "Les notes de cours"; ?> 
+                    </div>
+                   
+                <?php    wp_nav_menu(array(
+                        "menu"=>$menu2,
+                        "container"=>"nav"
+                    ))
+                    
+                ?>
+            </div>
+            
         </div>
-        
-    </div>
     
     
     </main>
--- readme.md
+++ readme.md
@@ -24,4 +24,5 @@
 - ajout d'un message pour la page 404 
 - ajout de is_404() dans le if dans header.php pour ajouter "no-aside"
 - ajout de la barre recherche sur la page 404 
-- 
+- ajout des menus cours et note 4w4 sur la page 404 
+
