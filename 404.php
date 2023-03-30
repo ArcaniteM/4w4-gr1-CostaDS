4e94f529390d7fab121957d22da8531c8834785b
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-03-30 09:28:15 -0400

    scss import dans layout pour 404

--- 404.php
+++ 404.php
@@ -1,11 +1,11 @@
 <?php /** index par defaut du theme */ ?>
 <?php get_header(); ?>
 
-    <main class="site__main">
+    <main class="site__main page404">
     <pres>404.php</pres>
         <h1>Bienvenue sur 4W4</h1>
         <h1>Erreur 404</h1>
-        <p>Page introuvable, veuillez tenter une recherche</p>
+        <p class="msg404">Page introuvable, veuillez tenter une recherche</p>
     </main>
     <?php get_footer(); ?>
 </body>
