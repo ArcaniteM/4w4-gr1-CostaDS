3738de8f8aee39181899aae95b889f0d6e37a0f3
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-04-25 11:48:01 -0400

    Ajout des evenements

--- front-page.php
+++ front-page.php
@@ -4,6 +4,14 @@
     <main class="site__main">
         <pre>front-page.php</pre>
         <h1>Bienvenue sur 4W4</h1>
+        <h2>Les evenements a venir</h2>
+        <section class="blocflex">
+        
+            <?php wp_nav_menu(array(
+                "menu" => "evenement",
+                "container" => "nav"
+            )); ?>
+        </section>
         <section class="blocflex">
             <?php if(have_posts()):
                 while (have_posts()): the_post();  
