c79da9f0d1ba401f3a257cd1a38011752888a3cb
Costa Dan Sebastian <e1171462@cmaisonneuve.qc.ca>
2023-05-23 10:27:39 -0400

    Ajout des articles pour la categorie epreuve finale

--- header.php
+++ header.php
@@ -10,7 +10,7 @@
 </head>
 <?php
 $nouvel_class="";
- if (is_front_page() || is_404()) {
+ if (is_front_page() || is_404()/* || is_categorie_ef()*/) {
     $nouvel_class = "no-aside";
  }?> 
 
---
+++ template-parts/categorie-ef.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * «template part» gabarit categorie-cours permet d'afficher un article «bloc»
+ * qui s'intègre dans la liste des cours qu'accède avec category/cours
+ * 
+ */
+$titre = get_the_title();
+if (substr($titre,0,1) == '0'){$titre = substr($titre,1);}
+// retirer le premier du titre d'article de catégorie note-4w4
+// if (substr($titre,0,1) == '0') {}...
+?>
+<article class="blocflex__article">
+    <h5><a href="<?php the_permalink(); ?>"> <?= $titre; ?></a></h5>
+    <p><?= wp_trim_words(get_the_excerpt(), 15) ?></p>
+</article>
+<?php
+// the excerpt()  // le résumé du post
+// the_content() // affiche le contenu complet  du post
+
+
+?>
